2fedfb03ffc6f0bd9942c767d031f72ed53e518d
Ascendzor <wow.tmare@gmail.com>
2016-07-08 15:33:43 +1200

    working

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ apk upgrade
 echo 'http://dl-4.alpinelinux.org/alpine/latest-stable/main/' >> /etc/apk/repositories
 apk update
 apk add redis
+
+
+mkdir /etc/service/redis
+mv /install/redis-run /etc/service/redis/run
