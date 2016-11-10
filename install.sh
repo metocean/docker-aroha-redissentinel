e2531d4d1c76415f123b00f50b6f2a3932bf4281
Mitchell Burton <m.burton@metocean.co.nz>
2016-11-11 11:50:57 +1300

    Pin redis version.

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ apk upgrade
 
 
 echo 'http://dl-4.alpinelinux.org/alpine/latest-stable/main/' >> /etc/apk/repositories
+echo 'http://dl-4.alpinelinux.org/alpine/edge/main/' >> /etc/apk/repositories
+
 apk update
-apk add redis
+apk add redis=3.2.3-r0
 
 
 mkdir /etc/service/redis
